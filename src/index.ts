2aad331d2d84e9b11f7131f84995856cbaa131d6
Joseph <jsphkhan@gmail.com>
2023-03-02 20:39:45 +0400

    initial commiit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import isClient from "./utils/ssr/isClient";
+import isServer from "./utils/ssr/isServer";
+import findQitafPoints from "./utils/qitaf/findQitafPoints";
+import getFormData from "./utils/document/getFormData";
+import getAddOnNotFound from "./utils/error/getAddOnNotFound";
+
+export {
+    isClient,
+    isServer,
+    findQitafPoints,
+    getFormData,
+    getAddOnNotFound
+}
